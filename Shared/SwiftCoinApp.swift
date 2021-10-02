154e2c3f37d0b0f1e41dfef86547bf5fe031f325
John B <jbethancourt.ctr@revacomm.com>
2021-10-02 13:45:41 -1000

    Initial Commit

---
+++ Shared/SwiftCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCoinApp.swift
+//  Shared
+//
+//  Created by John Bethancourt on 10/2/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
